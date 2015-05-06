f224f37831d73449e87459091d1fc69024ac41f5
Arjun <arjunthought@gmail.com>
2015-05-06 19:21:52 +0530

    httpclient intermediate checkin

--- httpClient/app/src/main/java/com/example/arjun/httpclient/httpClient.java
+++ httpClient/app/src/main/java/com/example/arjun/httpclient/httpClient.java
@@ -1,19 +1,83 @@
 package com.example.arjun.httpclient;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.SoftReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class httpClient extends ActionBarActivity {
+
+    private static final String url = "www.google.com";
+    private static final String DEBUG_TAG = "httpClient";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http_client);
+
+        // Check for network availability
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Network available, initiate a async task in background
+            new DownloadMenuTask().execute(url); //equivalent to declaring an object of type Down.. and calling the method execute() on it
+        } else {
+            Toast.makeText(this, "No network connection available", Toast.LENGTH_LONG).show();
+        }
     }
 
+    private class DownloadMenuTask extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0].toString());
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+        }
+
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+
+        URL url = new URL(myurl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000); // milliseconds
+        conn.setConnectTimeout(15000); //milliseconds
+        conn.setRequestMethod("GET"); //this is def also
+        conn.setDoInput(true);
+
+        //Start the query
+        conn.connect();
+        int response = conn.getResponseCode();
+        Log.d(DEBUG_TAG, "The response is: " + response);
+
+        is = conn.getInputStream();
+
+
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
